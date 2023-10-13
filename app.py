1f911e9d73778de37197f2e90b06ae6c51c988c1
Syed Addan <syedaddan@gmail.com>
2023-10-13 15:40:12 +0500

    Another commit

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 with open('iris_classifier.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def classify_iris():
     result = ''
-    
     if request.method == 'POST':
         try:
             # Get user input from the form
@@ -21,16 +21,20 @@ def classify_iris():
             petal_width = float(request.form['petal_width'])
 
             # Make a prediction using the loaded model
-            input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+            input_data = np.array([[sepal_length,
+                                    sepal_width,
+                                    petal_length,
+                                    petal_width]])
             prediction = model.predict(input_data)[0]
 
             # Map the numeric prediction to Iris species
             iris_species = ['Setosa', 'Versicolor', 'Virginica']
-            result = f'The predicted Iris species is {iris_species[prediction]}'
+            result = f'Predicted species is {iris_species[prediction]}'
         except ValueError:
             result = 'Invalid input. Please enter valid numeric values.'
 
     return render_template('index.html', result=result)
 
+
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port=4000)
+    app.run(debug=True, host="0.0.0.0", port=4000)
